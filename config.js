d2544cef6dc750375afb220d06a5bf927874e7ae
Akarshana0 <pansiluakarshana0@gmail.com>
2026-02-07 18:56:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+const BOT_NAME_FANCY = 'ALONE MD';
+
+const config = {
+  AUTO_VIEW_STATUS: 'true',
+  AUTO_LIKE_STATUS: 'true',
+  AUTO_RECORDING: 'false',
+  AUTO_LIKE_EMOJI: ['☘️','💗','🫂','🙈','🍁','🙃','🧸','😘','🏴‍☠️','👀','❤️‍🔥'],
+  PREFIX: '.',
+  MAX_RETRIES: 3,
+  GROUP_INVITE_LINK: 'https://chat.whatsapp.com/Hdf6sdiJKo48zsPHgsIbkg',
+  RCD_IMAGE_PATH: 'https://files.catbox.moe/1m4824.jpeg',
+  NEWSLETTER_JID: '120363404236484560@newsletter',
+  OTP_EXPIRY: 300000,
+  WORK_TYPE: 'public',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '94703229822',
+  CHANNEL_LINK: 'https://whatsapp.com/channel/0029VbBTCb6E50Ub9aV3133f',
+  BOT_NAME: 'ALONE MD',
+  BOT_VERSION: '2.0.0V',
+  OWNER_NAME: 'AKARSHANA',
+  IMAGE_PATH: 'https://files.catbox.moe/1m4824.jpeg',
+  BOT_FOOTER: '> *©ᴘᴏᴡᴇʀᴅ ʙʏ ALONE MD*',
+  BUTTON_IMAGES: { ALIVE: 'https://files.catbox.moe/1m4824.jpeg' }
+};
